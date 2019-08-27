6a641dcc9f81bfc3029a64dd68444dbfe1788831
Jack Lindamood <cep221@gmail.com>
2019-08-27 13:58:52 -0700

    example: add example file

---
+++ example/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/cep21/serviceexecutor"
+	"os"
+	"syscall"
+	"time"
+)
+
+type Printer struct {
+	shutdownSignal chan struct{}
+	runDone chan struct{}
+}
+
+func (p *Printer) Setup() error {
+	p.shutdownSignal = make(chan struct{})
+	p.runDone = make(chan struct{})
+	return nil
+}
+
+func (p *Printer) Run() error {
+	defer close(p.runDone)
+	for {
+		select {
+		case <- p.shutdownSignal:
+			return nil
+		case <- time.After(time.Second):
+			fmt.Println("hello", time.Now())
+		}
+	}
+}
+
+func (p *Printer) Shutdown(ctx context.Context) error {
+	close(p.shutdownSignal)
+	<- p.runDone
+	return nil
+}
+
+var _ serviceexecutor.Setupable = &Printer{}
+
+func main() {
+	m := &serviceexecutor.Multi{
+		Services: []serviceexecutor.Service{&Printer{}},
+	}
+	serviceexecutor.ShutdownOnSignals(m, time.Second, syscall.SIGTERM, syscall.SIGINT)
+	fmt.Println("Starting to run.  PID=", os.Getpid())
+	if err := m.Run(); err != nil {
+		panic("Should not panic")
+	}
+	fmt.Println("Done running")
+}
